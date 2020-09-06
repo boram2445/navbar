36301ab8d803b860f16ccc2c8d0384d558ef748c
boram2445 <68495264+boram2445@users.noreply.github.com>
2020-09-06 16:37:35 +0900

    navbar some add

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 const ToggleBtn = document.querySelector(".toggle");
 const menu = document.querySelector(".menu");
 const btn = document.querySelector(".btn");
+
 ToggleBtn.addEventListener("click", () => {
   menu.classList.toggle("active");
   btn.classList.toggle("active");
