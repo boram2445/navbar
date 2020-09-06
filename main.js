9b5661ad889b8e8ba018aca4e7dd6f1107094eb7
boram2445 <68495264+boram2445@users.noreply.github.com>
2020-09-06 16:20:25 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const ToggleBtn = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+const btn = document.querySelector(".btn");
+ToggleBtn.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  btn.classList.toggle("active");
+});
